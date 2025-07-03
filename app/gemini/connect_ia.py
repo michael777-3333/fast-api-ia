4a0864818d7e27821d237ead36e3d3cec2e993e8
Michael Stiven Sarmiento Porras <131264034+michael777-3333@users.noreply.github.com>
2025-07-02 23:23:01 -0500

    fix: remove deprecated

--- app/gemini/connect_ia.py
+++ app/gemini/connect_ia.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
